c5f8ac7f95931f3ea05d126de018c32e588c2e65
David Ogbeide <boyowao@gmail.com>
2017-12-22 23:02:19 -0500

    Centralize all static media files

---
+++ assets/innerspace/js/master.js
@@ -0,0 +1,16 @@
+// var color = '';
+// $('a').hover(function() {
+//     var x = $(this).css('color');
+//     hexc(x);
+//     alert(color);
+// })
+//
+// function hexc(colorval) {
+//     var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+//     delete(parts[0]);
+//     for (var i = 1; i <= 3; ++i) {
+//         parts[i] = parseInt(parts[i]).toString(16);
+//         if (parts[i].length == 1) parts[i] = '0' + parts[i];
+//     }
+//     color = '#' + parts.join('');
+// }
